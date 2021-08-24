fa33a5b5594e4906345a317a314bcd64996a7d38
Uros Drca <urosdrca22@gmail.com>
2021-08-24 13:35:02 +0200

    finished the game function

--- rps.js
+++ rps.js
@@ -6,17 +6,63 @@ function computerPlay(){
 function playRound(computerChoice, playerChoice){
 
     let outcome = "";
-
+    
     if ((playerChoice == 'rock' && computerChoice == 'scissors') ||
         (playerChoice == 'paper' && computerChoice == 'rock') ||
         (playerChoice == 'scissors' && computerChoice == 'scissors')){
-            outcome = `You win - ${playerChoice} beats ${computerChoice}.`
+            outcome = 'win'
+            console.log(`You win - ${playerChoice} beats ${computerChoice}.`)
+        }
+        else if(playerChoice == computerChoice){
+            outcome = 'draw'
+            console.log(`It's a draw - you both picked ${playerChoice}.`)
         }
         else{
-            outcome = 'shit'
+            outcome = 'loss'
+            console.log(`You lose - ${playerChoice} loses to ${computerChoice}.`)
         }
         return outcome
     }
 
-    const playerSelection = 'rock'
-    console.log(playRound(computerPlay(), playerSelection)) 
+
+
+function game(){
+
+    playerScore = 0;
+    computerScore = 0;
+    winner = "";
+    
+    while ((playerScore < 5) && (computerScore < 5)){
+
+        const playerPlay = prompt('Rock, paper, or scissors?').toLowerCase();
+        let result = playRound(computerPlay(), playerPlay)  
+        
+        if (result == 'win'){
+            playerScore += 1;
+            console.log(`Player Score: ${playerScore} | ${computerScore} Computer Score`)
+        }
+        else if (result == 'loss'){
+            computerScore += 1;
+            console.log(`Player Score: ${playerScore} | ${computerScore} Computer Score`)
+        }
+        else{
+            console.log(`Player Score: ${playerScore} | ${computerScore} Computer Score`)
+            continue;
+        }
+
+        if (playerScore == 5){
+            winner = 'Player'
+            console.log('Player is victorious!')
+        }
+        else if (computerScore == 5){
+            winner = 'Computer'
+            console.log('Computer is victorious!')
+        }
+        else{
+            continue;
+        }
+
+        
+    }
+}
+game();
